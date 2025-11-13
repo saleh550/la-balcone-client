b0095a3d971caeefcd7bade3a325d6c75a4d182e
saleh550 <salehf550@gmail.com>
2025-11-13 15:55:47 +0200

    scrolling fixed

--- src/components/customs/ScrollToTop.tsx
+++ src/components/customs/ScrollToTop.tsx
@@ -1,15 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
-
+  const lastPath = useRef(pathname);
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth", // or "smooth" if you prefer
-    });
+    if (lastPath.current !== pathname) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      lastPath.current = pathname;
+    }
   }, [pathname]);
 
   return null;
